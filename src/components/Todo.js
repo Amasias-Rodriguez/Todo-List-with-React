2b6612af89354195d381b76e2d4a33ed69999536
Amasias-Rodriguez <rodamasias01@gmail.com>
2023-10-21 21:40:22 -0600

    update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/fontawesome-free'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
-export const Todo = () => {
+export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
     return (
         <div className='Todo'>
-            <p>Go to School</p>
+            <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ""}`}>{task.task}</p>
             <dic>
-                <FontAwesomeIcon icon={faPenToSquare} />
-                <FontAwesomeIcon icon={faTrash} />
+                <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+                <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
             </dic>
         </div>
     )
